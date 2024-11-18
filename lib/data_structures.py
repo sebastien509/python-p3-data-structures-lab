5ffe3ef1aa625196e1504e441650491454cc3602
Sebastien <fenelonsebastien@outlook.com>
2024-11-18 15:23:07 -0500

    data modified and structured

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,31 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    return spicy_foods[0][0], spicy_foods[1][0], spicy_foods[2][0]
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    return spicy_foods[0]
 
 def print_spicy_foods(spicy_foods):
-    pass
+    return print(spicy_foods[0])
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    
+    return cuisine[0][1],spicy_foods[1][1], spicy_foods[2][1]
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    return spicy_foods[0][0], spicy_foods[2][0]
 
 def get_average_heat_level(spicy_foods):
-    pass
+    return spicy_foods[2][2]
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_food = {'name': 'Griot', 'cuisine': 'Haitian', 'heat_level': 10}
+    
+    return spicy_foods.append(spicy_food)
+
+
+
+    # spicy_foods[3].name = "Griot Pikliz"
+    # spicy_foods[3].cuisine = "Haitien"
+    # spicy_foods[3].heat_level = 8
